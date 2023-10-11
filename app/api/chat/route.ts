fabd08bb0624ea7ddcfca90436fbaeadf4d00783
yazid138 <ma39isy@gmail.com>
2023-10-11 09:42:38 +0700

    menambahkan cognitif search

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,75 +1,89 @@
-import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import {kv} from '@vercel/kv'
+import {OpenAIStream, StreamingTextResponse} from 'ai'
 
-import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
-import OpenAI from 'openai'
-import { ChatCompletionCreateParamsBase } from 'openai/resources/chat/completions'
+import {auth} from '@/auth'
+import {nanoid} from '@/lib/utils'
+import {OpenAIClient, AzureKeyCredential} from '@azure/openai'
+import {ChatCompletionCreateParamsBase} from 'openai/resources/chat/completions'
+import {setLogLevel} from "@azure/logger";
 
 export const runtime = 'edge'
 
-const resource = process.env.AZURE_RESOURCE_GROUP
-const modelType = process.env.AZURE_MODEL_TYPE
+const openAIResource = process.env.AZURE_OPENAI_RESOURCE_NAME
+const deployName = process.env.AZURE_OPENAI_DEPLOY_NAME || ""
 const modelName = process.env
-  .AZURE_MODEL_NAME as ChatCompletionCreateParamsBase['model']
+    .AZURE_OPENAI_MODEL_NAME as ChatCompletionCreateParamsBase['model']
+const cognitifResource = process.env.AZURE_COGNITIF_RESOURCE_NAME
+const cognitifIndex = process.env.AZURE_COGNITIF_INDEX
 
-const apiKey = process.env.AZURE_OPENAI_API_KEY
-if (!apiKey) {
-  throw new Error('AZURE_OPENAI_API_KEY is missing from the environment.')
+const openAIApiKey = process.env.AZURE_OPENAI_API_KEY
+if (!openAIApiKey) {
+    throw new Error('AZURE_OPENAI_API_KEY is missing from the environment.')
+}
+const cognitifApiKey = process.env.AZURE_COGNITIF_API_KEY
+if (!cognitifApiKey) {
+    throw new Error('AZURE_OPENAI_API_KEY is missing from the environment.')
 }
 
-const openai = new OpenAI({
-  apiKey,
-  baseURL: `https://${resource}.openai.azure.com/openai/deployments/${modelType}`,
-  defaultQuery: { 'api-version': '2023-06-01-preview' },
-  defaultHeaders: { 'api-key': apiKey }
-})
+setLogLevel("info");
+
+const openai = new OpenAIClient(`https://${openAIResource}.openai.azure.com/`, new AzureKeyCredential(openAIApiKey));
 
 export async function POST(req: Request) {
-  const json = await req.json()
-  const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
+    const json = await req.json()
+    const {messages, previewToken} = json
+    const userId = (await auth())?.user.id
 
-  if (!userId) {
-    return new Response('Unauthorized', {
-      status: 401
-    })
-  }
-
-  const res = await openai.chat.completions.create({
-    model: modelName,
-    messages,
-    temperature: 0.7,
-    stream: true
-  })
-
-  const stream = OpenAIStream(res['response'], {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
+    if (!userId) {
+        return new Response('Unauthorized', {
+            status: 401
+        })
     }
-  })
 
-  return new StreamingTextResponse(stream)
+    const res = openai.listChatCompletions(deployName, messages, {
+        model: modelName,
+        temperature: 0.7,
+        stream: true,
+        maxTokens: 128,
+        azureExtensionOptions: {
+            extensions: [{
+                type: "AzureCognitiveSearch",
+                parameters: {
+                    endpoint: `https://${cognitifResource}.search.windows.net`,
+                    key: cognitifApiKey,
+                    indexName: cognitifIndex,
+                }
+            }]
+        }
+    })
+    // @ts-ignore
+    const stream = OpenAIStream(res, {
+        async onCompletion(completion) {
+            const title = json.messages[0].content.substring(0, 100)
+            const id = json.id ?? nanoid()
+            const createdAt = Date.now()
+            const path = `/chat/${id}`
+            const payload = {
+                id,
+                title,
+                userId,
+                createdAt,
+                path,
+                messages: [
+                    ...messages,
+                    {
+                        content: completion,
+                        role: 'assistant'
+                    }
+                ]
+            }
+            await kv.hmset(`chat:${id}`, payload)
+            await kv.zadd(`user:chat:${userId}`, {
+                score: createdAt,
+                member: `chat:${id}`
+            })
+        }
+    })
+
+    return new StreamingTextResponse(stream)
 }
