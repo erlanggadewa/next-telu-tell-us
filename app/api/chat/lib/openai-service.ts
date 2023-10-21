06d9a053d4bb49ea60cde0227c317d94aff3baf0
erlanggadewa <erlanggadewa7@gmail.com>
2023-10-21 23:22:57 +0700

    FEATURE : finish milestone 1 to create the service chat ai

---
+++ app/api/chat/lib/openai-service.ts
@@ -0,0 +1,32 @@
+import OpenAI from 'openai'
+import { appConfig } from '../../config'
+
+export class OpenAiService {
+  chatClient: OpenAI
+  embeddingsClient: OpenAI
+
+  constructor() {
+    const openAiUrl = `https://${appConfig.azureOpenAiService}.openai.azure.com/`
+    const commonOptions = {
+      apiKey: appConfig.azureOpenAiKey,
+      defaultQuery: { 'api-version': appConfig.azureOpenAiApiVersion },
+      defaultHeaders: { 'api-key': appConfig.azureOpenAiKey }
+    }
+
+    this.chatClient = new OpenAI({
+      ...commonOptions,
+      baseURL: `${openAiUrl}/openai/deployments/${appConfig.azureOpenAiChatGptDeployment}`
+    })
+
+    this.embeddingsClient = new OpenAI({
+      ...commonOptions,
+      baseURL: `${openAiUrl}/openai/deployments/${appConfig.azureOpenAiEmbeddingDeployment}`
+    })
+  }
+  getChat() {
+    return this.chatClient.chat
+  }
+  getEmbeddings() {
+    return this.embeddingsClient.embeddings
+  }
+}
