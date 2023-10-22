fe1578cfa85c2bd90011dffab2bd858e11de0cd0
yazid138 <ma39isy@gmail.com>
2023-10-23 00:16:18 +0700

    add SignIn Page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,16 +1,69 @@
+import {Metadata} from "next";
+import Image from "next/image";
+import {UserAuthForm} from "@/components/user-auth-form";
+import {redirect} from "next/navigation";
 import { auth } from '@/auth'
-import { LoginButton } from '@/components/login-button'
-import { redirect } from 'next/navigation'
 
-export default async function SignInPage() {
-  const session = await auth()
-  // redirect to home if user is already logged in
-  if (session?.user) {
-    redirect('/')
-  }
-  return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-      <LoginButton />
-    </div>
-  )
+import PutiImage from "../../assets/images/puti.png";
+import TellUs from "../../assets/images/tell-us.png";
+import Vector from "../../assets/images/vector.png";
+import Ellipse from "../../assets/images/ellipse.png";
+import GedungRektorat from "../../assets/images/gedung-rektorat.png";
+
+export const metadata: Metadata = {
+    title: "Authentication",
+    description: "Authentication.",
+};
+
+export default async function AuthenticationPage() {
+    const session = await auth()
+    if (session?.user) {
+        redirect('/')
+    }
+    return (
+        <div
+            className="container h-screen md:grid lg:max-w-none lg:grid-cols-5 lg:px-0 lg:bg-gradient-to-b lg:from-[#ED1E28] lg:to-red-900">
+            <div
+                className="absolute inset-0 hidden lg:block w-full h-full opacity-10 bg-no-repeat bg-cover"
+                style={{
+                    backgroundImage: `url(${GedungRektorat.src})`,
+                }}
+            />
+
+            <div
+                className="h-screen flex justify-between items-center flex-col col-span-2 lg:bg-white lg:rounded-tr-[45px] lg:drop-shadow-2xl">
+                <div/>
+                <div className="mx-auto flex flex-col sm:w-[500px] lg:w-[400px] space-y-6">
+                    <div className="flex flex-col space-y-10 ">
+                        <h1 className="text-4xl font-semibold md:font-bold tracking-tight">
+                            Halo Telutizen!
+                            <br/>
+                            Selamat Datang
+                        </h1>
+                        <p className="text-lg text-muted-foreground">
+                            Single Sign On <span className="font-bold">(SSO)</span> login
+                        </p>
+                    </div>
+                    <UserAuthForm/>
+                </div>
+                <footer className="flex flex-col md:flex-row items-center mb-10">
+                    Powered By: <Image className="md:ml-3 mt-4 md:mt-0" src={PutiImage}
+                                       alt="Direktorat Pusat Teknologi Informasi Universitas Telkom"/>
+                </footer>
+            </div>
+
+            <div
+                className="relative hidden h-full flex-col p-10 text-white dark:border-r lg:flex lg:items-center lg:justify-center col-span-3">
+                <div className="text-center space-y-6 z-10">
+                    <Image className="m-auto" src={TellUs} alt="logo tell-us" width={350}/>
+                    <h3 className="text-xl italic">Your gateway to academic excellence</h3>
+                </div>
+                <div className="absolute w-full h-full inset-0">
+                    <Image className="absolute top-8 left-8" src={Vector} alt="vector"/>
+                    <Image className="absolute top-0 right-0" src={Ellipse} alt="vector" width={75}/>
+                    <Image className="absolute bottom-8 right-8" src={Vector} alt="vector"/>
+                </div>
+            </div>
+        </div>
+    );
 }
--- components/providers.tsx
+++ components/providers.tsx
@@ -1,15 +1,18 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
+import {ThemeProvider as NextThemesProvider} from 'next-themes'
+import {ThemeProviderProps} from 'next-themes/dist/types'
+import {SessionProvider} from "next-auth/react";
 
-import { TooltipProvider } from '@/components/ui/tooltip'
+import {TooltipProvider} from '@/components/ui/tooltip'
 
-export function Providers({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props}>
-      <TooltipProvider>{children}</TooltipProvider>
-    </NextThemesProvider>
-  )
+export function Providers({children, ...props}: ThemeProviderProps) {
+    return (
+        <NextThemesProvider {...props}>
+            <SessionProvider>
+                <TooltipProvider>{children}</TooltipProvider>
+            </SessionProvider>
+        </NextThemesProvider>
+    )
 }
