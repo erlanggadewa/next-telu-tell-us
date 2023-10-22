7e3c2335924741807fd496549588406c7e83b432
yazid138 <ma39isy@gmail.com>
2023-10-22 20:42:20 +0700

    setting buat ai

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,22 +1,13 @@
 import { auth } from '@/auth'
-import { setLogLevel } from '@azure/logger'
-import { AzureKeyCredential, OpenAIClient } from '@azure/openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { appConfig } from '../config'
+import {nanoid, OpenAIStream, StreamingTextResponse} from 'ai'
 import { ChatApproach } from './lib/chat-read-retrieve-read'
+import {kv} from "@vercel/kv";
 
 export const runtime = 'edge'
 
-setLogLevel('warning')
-
-const openai = new OpenAIClient(
-  `https://${appConfig.azureOpenAiService}.openai.azure.com/`,
-  new AzureKeyCredential(appConfig.azureOpenAiKey)
-)
-
 export async function POST(req: Request) {
   const json = await req.json()
-  const { messages, approach, previewToken, cognitif } = json
+  const { messages } = json
   const userId = (await auth())?.user.id
 
   if (!userId) {
@@ -26,34 +17,36 @@ export async function POST(req: Request) {
   }
 
   const chatApproach = new ChatApproach()
-  const finalMsg = await chatApproach.baseRun(messages, approach)
+  const finalMsg = await chatApproach.baseRun(messages, {
+    suggest_followup_questions: true,
+    retrieval_mode: 'text'
+  })
 
-  // @ts-ignore
   const stream = OpenAIStream(finalMsg, {
     async onCompletion(completion) {
-      //   const title = json.messages[0].content.substring(0, 100)
-      //   const id = json.id ?? nanoid()
-      //   const createdAt = Date.now()
-      //   const path = `/chat/${id}`
-      //   const payload = {
-      //     id,
-      //     title,
-      //     userId,
-      //     createdAt,
-      //     path,
-      //     messages: [
-      //       ...messages,
-      //       {
-      //         content: completion,
-      //         role: 'assistant'
-      //       }
-      //     ]
-      //   }
-      //   await kv.hmset(`chat:${id}`, payload)
-      //   await kv.zadd(`user:chat:${userId}`, {
-      //     score: createdAt,
-      //     member: `chat:${id}`
-      //   })
+    //     const title = json.messages[0].content.substring(0, 100)
+    //     const id = json.id ?? nanoid()
+    //     const createdAt = Date.now()
+    //     const path = `/chat/${id}`
+    //     const payload = {
+    //       id,
+    //       title,
+    //       userId,
+    //       createdAt,
+    //       path,
+    //       messages: [
+    //         ...messages,
+    //         {
+    //           content: completion,
+    //           role: 'assistant'
+    //         }
+    //       ]
+    //     }
+    //     await kv.hmset(`chat:${id}`, payload)
+    //     await kv.zadd(`user:chat:${userId}`, {
+    //       score: createdAt,
+    //       member: `chat:${id}`
+    //     })
     }
   })
 
