3697968f78f06fbf417434382b40a0390bb70980
erlanggadewa <erlanggadewa7@gmail.com>
2023-11-16 22:37:34 +0700

    PATCHING :  change interface chat response

--- app/api/chat/document/route.ts
+++ app/api/chat/document/route.ts
@@ -1,46 +1,48 @@
-import {auth} from '@/auth'
-import {appConfig} from '@/config'
-import {
-    OpenAIStream,
-    StreamingTextResponse,
-} from 'ai'
+import { auth } from '@/auth'
+import { appConfig } from '@/config'
+import { OpenAiService } from '@/lib/openai-service'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
 import axios from 'axios'
-import {ChatCompletionMessageParam} from 'openai/resources'
-import {OpenAiService} from '@/lib/openai-service'
+import { ChatCompletionMessageParam } from 'openai/resources'
 
 const api = `${appConfig.apiUrl}/chat/citation`
 
 interface ChatResponse {
-    dataPoints: string[]
-    citationIds: string[]
-    bodyGenerateMsg: {
-        model: string
-        messages: ChatCompletionMessageParam[]
-        temperature: number
-        n: number
-        stream: boolean
-    }
+  dataPoints: string[]
+  citationSource: {
+    citationId: string
+    sourcePage: string
+    sourceFile: string
+  }[]
+  bodyGenerateMsg: {
+    model: string
+    messages: ChatCompletionMessageParam[]
+    temperature: number
+    n: number
+    stream: boolean
+  }
 }
 
 export async function POST(req: Request) {
-    const json = await req.json()
-    const {messages, citationId} = json
-    const userId = (await auth())?.user.id
+  const json = await req.json()
+  const { messages, citationId } = json
+  const userId = (await auth())?.user.id
 
-    if (!userId) {
-        return new Response('Unauthorized', {
-            status: 401
-        })
-    }
+  if (!userId) {
+    return new Response('Unauthorized', {
+      status: 401
+    })
+  }
 
-    const data: ChatResponse = (await axios.post(api, {messages, citationId})).data
-    const {bodyGenerateMsg} = data
+  const data: ChatResponse = (await axios.post(api, { messages, citationId }))
+    .data
+  const { bodyGenerateMsg } = data
 
-    const finalMsg = await new OpenAiService().chatClient.chat.completions.create(
-        bodyGenerateMsg
-    )
+  const finalMsg = await new OpenAiService().chatClient.chat.completions.create(
+    bodyGenerateMsg
+  )
 
-    const stream = OpenAIStream(finalMsg as any)
+  const stream = OpenAIStream(finalMsg as any)
 
-    return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream)
 }
