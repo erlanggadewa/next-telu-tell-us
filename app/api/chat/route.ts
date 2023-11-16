978573a092bb608b7fa98ee25510a2b1e4518e74
erlanggadewa <erlanggadewa7@gmail.com>
2023-11-16 22:37:34 +0700

    PATCHING :  change interface chat response

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,54 +1,57 @@
-import {auth} from '@/auth'
-import {appConfig} from '@/config'
+import { auth } from '@/auth'
+import { appConfig } from '@/config'
+import { OpenAiService } from '@/lib/openai-service'
 import {
-    OpenAIStream,
-    StreamingTextResponse,
-    experimental_StreamData
+  OpenAIStream,
+  StreamingTextResponse,
+  experimental_StreamData
 } from 'ai'
 import axios from 'axios'
-import {ChatCompletionMessageParam} from 'openai/resources'
-import {OpenAiService} from '@/lib/openai-service'
+import { ChatCompletionMessageParam } from 'openai/resources'
 
 const api = `${appConfig.apiUrl}/chat`
 
 interface ChatResponse {
-    dataPoints: string[]
-    citationIds: string[]
-    bodyGenerateMsg: {
-        model: string
-        messages: ChatCompletionMessageParam[]
-        temperature: number
-        n: number
-        stream: boolean
-    }
+  dataPoints: string[]
+  citationSource: {
+    citationId: string
+    sourcePage: string
+    sourceFile: string
+  }[]
+  bodyGenerateMsg: {
+    model: string
+    messages: ChatCompletionMessageParam[]
+    temperature: number
+    n: number
+    stream: boolean
+  }
 }
 
 export async function POST(req: Request) {
-    const json = await req.json()
-    const {messages} = json
-    const userId = (await auth())?.user.id
+  const json = await req.json()
+  const { messages } = json
+  const userId = (await auth())?.user.id
 
-    if (!userId) {
-        return new Response('Unauthorized', {
-            status: 401
-        })
-    }
-
-    const data: ChatResponse = (await axios.post(api, {messages})).data
-    const {bodyGenerateMsg, dataPoints, citationIds} = data
-
-    const finalMsg = await new OpenAiService().chatClient.chat.completions.create(
-        bodyGenerateMsg
-    )
-
-    const appendData = new experimental_StreamData()
-    appendData.append({dataPoints, citationIds})
-
-    const stream = OpenAIStream(finalMsg as any, {
-        experimental_streamData: true,
-        onFinal: () => appendData.close()
-        ,
+  if (!userId) {
+    return new Response('Unauthorized', {
+      status: 401
     })
+  }
 
-    return new StreamingTextResponse(stream, {}, appendData)
+  const data: ChatResponse = (await axios.post(api, { messages })).data
+  const { bodyGenerateMsg, dataPoints, citationSource } = data
+
+  const finalMsg = await new OpenAiService().chatClient.chat.completions.create(
+    bodyGenerateMsg
+  )
+
+  const appendData = new experimental_StreamData()
+  appendData.append({ dataPoints, citationSource })
+
+  const stream = OpenAIStream(finalMsg as any, {
+    experimental_streamData: true,
+    onFinal: () => appendData.close()
+  })
+
+  return new StreamingTextResponse(stream, {}, appendData)
 }
