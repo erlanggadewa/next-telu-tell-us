c8b436bfbf872949a18d52fb1aca6d71441fbe72
yazid138 <ma39isy@gmail.com>
2023-11-22 12:18:34 +0700

    view: menambahkan summary document

--- app/chat/document/[path]/components/pdf-viewer.tsx
+++ app/chat/document/[path]/components/pdf-viewer.tsx
@@ -1,80 +1,73 @@
 'use client'
 
-import { appConfig } from '@/config'
+import {appConfig} from '@/config'
 import axios from 'axios'
-import { MutableRefObject, useEffect, useRef, useState } from 'react'
+import {MutableRefObject, useEffect, useRef, useState} from 'react'
+import {bl} from "@upstash/redis/zmscore-10fd3773";
 
 const PdfViewer = ({
-  path,
-  citationId
-}: {
-  path: string
-  citationId?: string
+                       path,
+                       summary,
+                   }: {
+    path: string
+    summary: string,
 }) => {
-  const iframeRef = useRef() as MutableRefObject<HTMLIFrameElement>
-  const [rating, setRating] = useState(3)
-  const totalRating = 5
-  const [summary, setSummary] = useState('')
+    const iframeRef = useRef() as MutableRefObject<HTMLIFrameElement>
+    const jumlahKata = summary.split(' ').length
 
-  useEffect(() => {
-    axios.get(`/summary/${citationId}`).then(r => {
-      setSummary(r)
-    })
-  }, [citationId])
+    useEffect(() => {
+        fetch(`${appConfig.apiUrl}/blob-storage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                filename: path
+            })
+        })
+            .then(r => r.blob())
+            .then(b => (iframeRef.current.src = URL.createObjectURL(b)))
+            .catch(e => console.error(e))
+    }, [path])
 
-  useEffect(() => {
-    fetch(`${appConfig.apiUrl}/blob-storage`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        filename: path
-      })
-    })
-      .then(r => r.blob())
-      .then(b => (iframeRef.current.src = URL.createObjectURL(b)))
-      .catch(e => console.error(e))
-  }, [path])
-
-  return (
-    <div className="sticky top-16">
-      <iframe ref={iframeRef} title={path} className="w-full h-[70vh]" />
-      {/*<div className="flex items-center justify-between gap-4 m-4 mx-10">*/}
-      {/*    <div className="flex items-center">*/}
-      {/*        {[...Array(totalRating)].map((_, index) => index < rating ? (*/}
-      {/*                <StarFilledIcon className='text-yellow-500' key={index}/>*/}
-      {/*            ) : <StarIcon className='text-yellow-500' key={index}/>*/}
-      {/*        )}*/}
-      {/*    </div>*/}
-      {/*</div>*/}
-      {/*<div className="flex items-center justify-between gap-4 m-4 mx-10">*/}
-      {/*    <div className="flex items-center gap-2" onClick={() => {*/}
-      {/*    }}>*/}
-      {/*        <InfoCircledIcon className='text-gray-600'/>*/}
-      {/*        <p>Lihat Informasi</p>*/}
-      {/*    </div>*/}
-      {/*    <div className="flex gap-2">*/}
-      {/*        <div className="flex items-center gap-2">*/}
-      {/*            <HeartIcon*/}
-      {/*                className={cn('text-gray-600 cursor-pointer')}*/}
-      {/*            />*/}
-      {/*            <p>123</p>*/}
-      {/*        </div>*/}
-      {/*        <div className="flex items-center gap-2">*/}
-      {/*            <DownloadIcon/>*/}
-      {/*            <p>456</p>*/}
-      {/*        </div>*/}
-      {/*    </div>*/}
-      {/*</div>*/}
-      {/*<Separator/>*/}
-      <h1 className="mx-12 mt-4 font-bold text-center text-md">{path}</h1>
-      <p className="mt-2 mb-4 text-sm text-justify mx-14">
-        {summary}
-        <span className="font-semibold text-gray-500 text-md">(50 kata)</span>
-      </p>
-    </div>
-  )
+    return (
+        <div className="sticky top-16">
+            <iframe ref={iframeRef} title={path} className="w-full h-[70vh]"/>
+            {/*<div className="flex items-center justify-between gap-4 m-4 mx-10">*/}
+            {/*    <div className="flex items-center">*/}
+            {/*        {[...Array(totalRating)].map((_, index) => index < rating ? (*/}
+            {/*                <StarFilledIcon className='text-yellow-500' key={index}/>*/}
+            {/*            ) : <StarIcon className='text-yellow-500' key={index}/>*/}
+            {/*        )}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<div className="flex items-center justify-between gap-4 m-4 mx-10">*/}
+            {/*    <div className="flex items-center gap-2" onClick={() => {*/}
+            {/*    }}>*/}
+            {/*        <InfoCircledIcon className='text-gray-600'/>*/}
+            {/*        <p>Lihat Informasi</p>*/}
+            {/*    </div>*/}
+            {/*    <div className="flex gap-2">*/}
+            {/*        <div className="flex items-center gap-2">*/}
+            {/*            <HeartIcon*/}
+            {/*                className={cn('text-gray-600 cursor-pointer')}*/}
+            {/*            />*/}
+            {/*            <p>123</p>*/}
+            {/*        </div>*/}
+            {/*        <div className="flex items-center gap-2">*/}
+            {/*            <DownloadIcon/>*/}
+            {/*            <p>456</p>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<Separator/>*/}
+            <h1 className="mx-12 mt-4 font-bold text-center text-md">{path}</h1>
+            <p className="mt-2 mb-4 text-sm text-justify mx-14">
+                {summary}
+                <span className="font-semibold text-gray-500 text-md">({jumlahKata} kata)</span>
+            </p>
+        </div>
+    )
 }
 
 export default PdfViewer
--- app/chat/document/[path]/page.tsx
+++ app/chat/document/[path]/page.tsx
@@ -1,30 +1,38 @@
 import PdfViewer from '@/app/chat/document/[path]/components/pdf-viewer'
-import { v4 as uuid } from 'uuid'
-import { Chat } from './components/chat'
+import {v4 as uuid} from 'uuid'
+import {Chat} from './components/chat'
+import axios from "axios";
+import {appConfig} from "@/config";
 
 export interface DocumentPageProps {
-  params: {
-    path: string
-  }
-  searchParams?: {
-    citationId: string
-  }
+    params: {
+        path: string
+    }
+    searchParams?: {
+        citationId: string
+    }
 }
 
-const DocumentPage = async ({ params, searchParams }: DocumentPageProps) => {
-  const id = uuid()
-  return (
-    <div className="grid grid-cols-5 gap-4">
-      <div className="col-span-2">
-        <PdfViewer
-          path={decodeURI(params.path)}
-          citationId={searchParams?.citationId}
-        />
-      </div>
-      <div className="h-full col-span-3">
-        <Chat id={id} citationId={searchParams?.citationId} />
-      </div>
-    </div>
-  )
+const getSummary = async (citationId?: string) => {
+    if (!citationId) return ''
+    return (await axios.get(appConfig.apiUrl + '/cognitive-search/summary/' + citationId)).data
+}
+
+const DocumentPage = async ({params, searchParams}: DocumentPageProps) => {
+    const id = uuid()
+    const summary = await getSummary(searchParams?.citationId)
+    return (
+        <div className="grid grid-cols-5 gap-4">
+            <div className="col-span-2">
+                <PdfViewer
+                    path={decodeURI(params.path)}
+                    summary={summary}
+                />
+            </div>
+            <div className="h-full col-span-3">
+                <Chat id={id} citationId={searchParams?.citationId}/>
+            </div>
+        </div>
+    )
 }
 export default DocumentPage
