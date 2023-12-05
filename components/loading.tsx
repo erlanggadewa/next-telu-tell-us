af93a84e9ef5ca2a3f8b5b429466d33d43aff811
erlanggadewa <erlanggadewa7@gmail.com>
2023-12-05 21:08:15 +0700

    PATCHING : change size loading

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,10 +1,8 @@
-import LoadingGif from '@/assets/gif/loading.gif';
-import Image from "next/image";
+import LoadingGif from '@/assets/gif/loading.gif'
+import Image from 'next/image'
 
 const Loading = () => {
-    return (
-        <Image className="w-48" src={LoadingGif} alt="loading..."/>
-    );
-};
+  return <Image className="w-44" src={LoadingGif} alt="loading..." />
+}
 
-export default Loading;
+export default Loading
