12bca3bae2569e0078ae5ad4a4efc99d61085ac0
yazid138 <ma39isy@gmail.com>
2023-12-05 14:18:33 +0700

    view: perbaiki tampilan katalog

--- app/components/Tab.tsx
+++ app/components/Tab.tsx
@@ -1,30 +1,81 @@
 "use client"
 
+import {v4 as uuid} from "uuid";
 import TabComponent from '@/components/ui/tab'
-import Tab1 from "@/app/components/Tabs/Tab1";
-import Catalog from "@/components/catalog";
+import Catalog, {CatalogType} from "@/components/catalog";
+import useSWR from "swr";
+import {appConfig} from "@/config";
+import axios from "axios";
 
-const data = [
-    {
-        id: 'catalog',
-        name: 'Rekomendasi Buku',
-        content: <Catalog/>,
-        default: true
-    },
-    {
-        id: 2,
-        name: 'Tell-US Search',
-        content: <Tab1 title='Rekomendasi Untuk Anda' subTitle="Rekomendasi buku tugas akhir paling disukai"/>,
-    },
-    {
-        id: 3,
-        name: 'Tell-US Summary',
-        content: <Tab1 title='Rekomendasi Untuk Anda' subTitle="Rekomendasi buku tugas akhir paling disukai"/>,
-    },
-]
+const getCatalog = async (category: string) => {
+    const id = uuid()
+    try {
+        return (await axios.post(`${appConfig.apiUrl}/catalog/homepage`, {
+            id,
+            query: `artikel atau buku ${category}`,
+            context: {
+                retrieval_mode: 'text',
+                top: 3,
+            }
+        })).data;
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+}
+
 const Tab = () => {
+    const {data: dataTab1, isLoading: isLoadingTab1} = useSWR('catalog1',
+        async () => Promise.all([
+            {
+                category: 'Teknologi',
+                data: await getCatalog('Teknologi')
+            },
+            {
+                category: 'Bisnis',
+                data: await getCatalog('Bisnis')
+            }
+        ])) as { data: CatalogType[], isLoading: boolean }
+    const {data: dataTab2, isLoading: isLoadingTab2} = useSWR('catalog2',
+        async () => Promise.all([
+            {
+                category: 'Akuntansi',
+                data: await getCatalog('Akuntansi')
+            },
+            {
+                category: 'Sosial',
+                data: await getCatalog('Sosial')
+            }
+        ])) as { data: CatalogType[], isLoading: boolean }
+    const {data: dataTab3, isLoading: isLoadingTab3} = useSWR('catalog3',
+        async () => Promise.all([
+            {
+                category: 'Budaya',
+                data: await getCatalog('Budaya')
+            },
+            {
+                category: 'Politik',
+                data: await getCatalog('Politik')
+            }
+        ])) as { data: CatalogType[], isLoading: boolean }
     return (
-        <TabComponent data={data}/>
+        <TabComponent data={[
+            {
+                id: 'catalog',
+                name: 'Rekomendasi Buku',
+                content: <Catalog data={dataTab1} isLoading={isLoadingTab1}/>,
+                default: true
+            },
+            {
+                id: 2,
+                name: 'Tell-US Search',
+                content: <Catalog data={dataTab2} isLoading={isLoadingTab2}/>,
+            },
+            {
+                id: 3,
+                name: 'Tell-US Summary',
+                content: <Catalog data={dataTab3} isLoading={isLoadingTab3}/>,
+            },
+        ]}/>
     );
 };
 
--- components/catalog.tsx
+++ components/catalog.tsx
@@ -1,25 +1,22 @@
-import axios from "axios";
-import {appConfig} from "@/config";
 import {Separator} from "@/components/ui/separator";
 import Link from "next/link";
-import {v4 as uuid} from "uuid";
 import LogoSystem from "@/assets/svg/system.svg";
 import Image from "next/image";
 
-const getCatalog = async (category: string) => {
-    const id = uuid()
-    try {
-        return (await axios.post(`${appConfig.apiUrl}/catalog/homepage`, {
-            id,
-            query: `artikel atau buku ${category}`,
-            context: {
-                retrieval_mode: 'text',
-                top: 3,
-            }
-        })).data;
-    } catch (error: any) {
-        throw new Error(error.message);
-    }
+type CatalogDataType = {
+    judul: string,
+    subjek: string,
+    author: string,
+    jeniskatalog: number,
+    tahunterbit: number,
+    publisher_name: string,
+    publisher_city: string,
+    link: string
+}
+
+export type CatalogType = {
+    category: string,
+    data: CatalogDataType[]
 }
 
 const Card = ({data}: {
@@ -37,7 +34,7 @@ const Card = ({data}: {
     const {judul, author, subjek, jeniskatalog, link, publisher_city, publisher_name, tahunterbit} = data
     return <Link target="_blank" href={link}
                  className="flex items-center justify-center lg:w-[32%] border rounded px-4 py-3 gap-3">
-        <Image src={LogoSystem} alt={judul} className="w-28 bg-red-800 p-6 rounded" />
+        <Image src={LogoSystem} alt={judul} className="w-28 bg-red-800 p-6 rounded"/>
         <div className="w-full space-y-1">
             <h4 className="uppercase font-semibold tex-md">{subjek}</h4>
             <h5 className="text-sm font-semibold">{jeniskatalog}</h5>
@@ -47,27 +44,18 @@ const Card = ({data}: {
     </Link>
 }
 
-const Catalog = async () => {
-    const data = await Promise.all([
-        {
-            category: 'Teknologi',
-            data: await getCatalog('Teknologi'),
-        }, {
-            category: 'Bisnis',
-            data: await getCatalog('Bisnis'),
-        }
-    ])
+const Catalog = ({data, isLoading}: { data: CatalogType[], isLoading: boolean }) => {
     return (
         <div className="w-full container space-y-4 py-6">
             <h2 className="uppercase text-2xl font-bold mt-6">Rekomendasi untuk anda</h2>
             <p className="text-muted-foreground">Rekomendasi buku tugas akhir yang paling disukai</p>
             <Separator className="bg-muted-foreground w-full"/>
             <div className="text-left">
-                {data.map((e) =>
+                {data?.map((e) =>
                     <div key={e.category} className="mt-5">
                         <h3 className="text-xl font-bold uppercase">{e.category}</h3>
                         <div className="flex flex-wrap gap-4 mt-4">
-                            {e.data?.map((e2: any) =>
+                            {e.data.map((e2: any) =>
                                 <Card key={e2.category} data={e2}/>
                             )}
                         </div>
