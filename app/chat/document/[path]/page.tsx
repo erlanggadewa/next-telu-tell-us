c24d6c2fe15c24d0ea828169c618741b5c056a92
yazid138 <ma39isy@gmail.com>
2023-12-07 10:43:03 +0700

    fix: perbaikan citationId

--- app/chat/document/[path]/page.tsx
+++ app/chat/document/[path]/page.tsx
@@ -1,39 +1,46 @@
 import PdfViewer from '@/app/chat/document/[path]/components/pdf-viewer'
-import { appConfig } from '@/config'
+import {appConfig} from '@/config'
 import axios from 'axios'
-import { v4 as uuid } from 'uuid'
-import { Chat } from './components/chat'
+import {v4 as uuid} from 'uuid'
+import {Chat} from './components/chat'
 
 export interface DocumentPageProps {
-  params: {
-    path: string
-  }
-  searchParams?: {
-    citationId: string
-  }
+    params: {
+        path: string
+    }
+    searchParams?: {
+        citationId: string
+    }
 }
 
-const getSummary = async (citationId?: string) => {
-  if (!citationId) return ''
-  return (
-    await axios.get(
-      appConfig.apiUrl + '/cognitive-search/summary/' + citationId
+const getSummary = async (citationId?: string[]) => {
+    if (!citationId) return []
+    try {
+        return (
+            await axios.post(
+                appConfig.apiUrl + '/cognitive-search/summary/',
+                {citationId}
+            )
+        ).data
+    } catch (e) {
+        console.log(e)
+    }
+    return []
+}
+
+const DocumentPage = async ({params, searchParams}: DocumentPageProps) => {
+    const id = uuid()
+    const citationId = searchParams?.citationId.split(',')
+    const summary = await getSummary(citationId)
+    return (
+        <div className="grid lg:grid lg:grid-cols-5 lg:gap-4">
+            <div className="z-50 lg:col-span-2">
+                <PdfViewer path={decodeURI(params.path)} summary={summary} />
+            </div>
+            <div className="h-full lg:col-span-3">
+                <Chat id={id} citationId={citationId}/>
+            </div>
+        </div>
     )
-  ).data
-}
-
-const DocumentPage = async ({ params, searchParams }: DocumentPageProps) => {
-  const id = uuid()
-  const summary = await getSummary(searchParams?.citationId)
-  return (
-    <div className="grid lg:grid lg:grid-cols-5 lg:gap-4">
-      <div className="z-50 lg:col-span-2">
-        <PdfViewer path={decodeURI(params.path)} summary={summary} />
-      </div>
-      <div className="h-full lg:col-span-3">
-        <Chat id={id} citationId={searchParams?.citationId} />
-      </div>
-    </div>
-  )
 }
 export default DocumentPage
