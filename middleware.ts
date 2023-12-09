fc8bc3c07307dc7ea8079e5c11f2458b0591b412
erlanggadewa <erlanggadewa7@gmail.com>
2023-12-09 18:53:01 +0700

    FEATURE : add opengraph image

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,21 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from 'next-auth/middleware'
 
 export default withAuth({
-    pages: {
-        signIn: '/sign-in',
-    }
+  pages: {
+    signIn: '/sign-in'
+  }
 })
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - opengraph.png
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|opengraph.png).*)'
+  ]
+}
